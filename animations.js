bd7f020b8d4c0771b2de67c9bd7568b9164f2831
jenniferthorpe <jennifer.thorpe@hotmail.com>
2019-08-19 14:56:09 +0200

    lagt till bild

--- animations.js
+++ animations.js
@@ -4,6 +4,6 @@ anime({
   targets: elements,
   translateX: 250,
   rotate: "1turn",
-  backgroundColor: ["#3471eb", "#bd34eb"],
+  //   backgroundColor: ["#3471eb", "#bd34eb"],
   duration: 3000
 });
