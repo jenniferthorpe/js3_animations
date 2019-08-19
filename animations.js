18ee4ace35cc014f33d37fb6293834d2f589f58b
jenniferthorpe <jennifer.thorpe@hotmail.com>
2019-08-19 14:42:01 +0200

    first try

---
+++ animations.js
@@ -0,0 +1,9 @@
+let elements = document.querySelector("#animation");
+
+anime({
+  targets: elements,
+  translateX: 250,
+  rotate: "1turn",
+  backgroundColor: ["#3471eb", "#bd34eb"],
+  duration: 3000
+});
